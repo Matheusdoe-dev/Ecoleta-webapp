5f8ed764a97dda4ac7d16c3d4511e3ba9e8fbb2a
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-06-06 18:09:17 -0300

    :lipstick: feat: Some changes
    Imported Dropzone component

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -10,6 +10,9 @@ import apiIBGE from "../../services/ibge-api";
 // styles
 import { FormWrapper, Field, FieldGroup, ItemsGrid, Leaflet } from "./styles";
 
+// component
+import Dropzone from "./../Dropzone/index";
+
 // interfaces
 interface Item {
   id: number;
@@ -47,6 +50,7 @@ const Form = () => {
   const [selectedUf, setSelectedUf] = useState("0");
   const [selectedCity, setSelectedCity] = useState("0");
   const [selectedItems, setSelectedItems] = useState<number[]>([]);
+  const [selectedFile, setSelectedFile] = useState<File>();
   const [selectedPosition, setSelectedPosition] = useState<[number, number]>([
     0,
     0,
@@ -121,16 +125,19 @@ const Form = () => {
     const [latitude, longitude] = selectedPosition;
     const items = selectedItems;
 
-    const data = {
-      name,
-      email,
-      whatsapp,
-      uf,
-      city,
-      latitude,
-      longitude,
-      items,
-    };
+    const data = new FormData();
+    data.append("name", name);
+    data.append("email", email);
+    data.append("whatsapp", whatsapp);
+    data.append("uf", uf);
+    data.append("city", city);
+    data.append("latitude", String(latitude));
+    data.append("longitude", String(longitude));
+    data.append("items", items.join(","));
+    
+    if (selectedFile) {
+      data.append("image", selectedFile);
+    }
 
     await apiEcoleta.post("points", data);
 
@@ -157,6 +164,8 @@ const Form = () => {
         Cadastro do
         <br /> ponto de coleta
       </h1>
+
+      <Dropzone onFileUploaded={setSelectedFile} />
 
       <fieldset>
         <legend>
